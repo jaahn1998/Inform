23acece78ff1feb6caac33597f1e9fc8ec6a2ed1
jaahn1998 <31366549+jaahn1998@users.noreply.github.com>
2020-01-19 23:05:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function onSignIn(googleUser)
     var profile = googleUser.getBasicProfile();
     if (typeof(Storage) !== "undefined") {
         localStorage.setItem("name",profile.getName());
-        localStorage.setItem("pic",profile.getImageURL());
+        localStorage.setItem("pic",profile.getImageUrl());
     }
     window.location.replace("https://jaahn1998.github.io/Inform/two.html");
 }
